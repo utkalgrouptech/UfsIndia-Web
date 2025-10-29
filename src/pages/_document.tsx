cb1a966d75780eccb702752a60a2ccd217b7fd53
shubhashree500 <shreeshubha98@gmail.com>
2025-10-29 12:12:12 +0530

    adding meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* ✅ Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="uVZ6D0yOLx63jExAFBdN7ygYiCB_W_q7egrqpXBNdPE"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
